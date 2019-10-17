fde286d716e0d7bc62f9c37fe652b2b139630ca0
Tofol <tofolcanodaw2@gmail.com>
2019-10-17 13:41:30 +0200

    cosas

--- Actividad2.1/src/HtmlConstructor.java
+++ Actividad2.1/src/HtmlConstructor.java
@@ -1,13 +1,29 @@
 
 public class HtmlConstructor {
 
-	private String hastaBody = "";	//<html ... <body>
-	private String contenido = "";	//<h1> introduce una...
-	private String cierraBody = "</body>";
+	private String hastaBody = "<!DOCTYPE html>\n" + 
+			"<html>\n" + 
+			"	<head>\n" + 
+			"    	<title>TODO supply a title</title>\n" + 
+			"    	<meta charset=\"UTF-8\">\n" + 
+			"    	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" + 
+			"	</head>\n" + 
+			"	<body>";	//<html ... <body>
+	private String contenido = "<h1>¿Es segura tu contraseña?</h1>\n" + 
+			"    	<h3>Calcula cuanto tardaría un hacker en averiguar tu contraseña usando fuerza bruta</h3>";	//<h1> introduce una...
+	private String formulario ="<form method=\"GET\" action=\"Main\">\n" + 
+			"        	<input type=\"password\" name=\"password\" value=\"Escribe la contraseña\"/>\n" + 
+			"        	<p>¿Qué tecnología quieres comprobar?</p>\n" + 
+			"        	<input type=\"radio\" name=\"cpu\" value=\"cpu\"/>\n" + 
+			"        	<input type=\"radio\" name=\"gpu\" value=\"gpu\"/>\n" + 
+			"        	<input type=\"radio\" name=\"grid\" value=\"grid\"/>\n" + 
+			"        	<input type=\"submit\" value=\"Calcula\"/>\n" + 
+			"    	</form>";
 	private String abreResul = "<div id=\"resultado\">";
-	private String cierraResul = "</div>";
 	private String resul = "";
-	
+	private String cierraResul = "</div>";
+	private String cierraBody = "</body></html>";
+		
 	public HtmlConstructor() {
 		
 	}
@@ -19,7 +35,7 @@ public class HtmlConstructor {
 	}
 	
 	public String toString() {
-		return hastaBody + contenido + abreResul + resul + cierraResul + cierraBody;
+		return hastaBody + contenido +formulario + abreResul + resul + cierraResul + cierraBody;
 	}
 	
 }
--- Actividad2.1/src/PasswordsComunes.java
+++ Actividad2.1/src/PasswordsComunes.java
@@ -4,23 +4,28 @@ import java.util.Properties;
 public class PasswordsComunes {
 
 	private ArrayList<String> passwordsComunes = new ArrayList<String>();
-	
+
 	public PasswordsComunes(Properties prop) {
 		ArrayList<String> pas = new ArrayList<String>();
-		
+
 		for (String key : prop.stringPropertyNames()) {
 			pas.add(key);
 		}
+
+	}
+
+	public boolean esComun(String password) throws Exception{
+		if(password == null) {
+			throw new Exception();
+		} else {
+			for (String pas : passwordsComunes) {
+				if (pas.equals(password)) {
+					return true;
+				}
+			}
+			return false;
+		}
 		
 	}
-	
-	public boolean esComun(String password) {
-		for (String pas : passwordsComunes) {
-			if(pas.equals(password)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
+
 }
